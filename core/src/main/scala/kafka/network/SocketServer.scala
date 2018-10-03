9198f70dc7912314f1c98ec3610ed1569d4c968d
Ron Dagostino <rndgstn@gmail.com>
2018-10-03 17:42:43 -0400

    Respond to review: use else if

--- core/src/main/scala/kafka/network/SocketServer.scala
+++ core/src/main/scala/kafka/network/SocketServer.scala
@@ -710,21 +710,19 @@ private[kafka] class Processor(val id: Int,
             val header = RequestHeader.parse(receive.payload)
             if (header.apiKey() == ApiKeys.SASL_HANDSHAKE && channel.maybeBeginServerReauthentication(receive, time.nanoseconds))
               trace(s"Begin re-authentication: $channel")
-            else {
-              if (channel.serverAuthenticationSessionExpired(time.nanoseconds)) {
-                channel.disconnect()
-                debug(s"Disconnected expired channel: $channel : $header")
-                expiredConnectionsKilledCount.record(null, 1, 0)
-              } else {
-                val connectionId = receive.source
-                val context = new RequestContext(header, connectionId, channel.socketAddress,
-                  channel.principal, listenerName, securityProtocol)
-                val req = new RequestChannel.Request(processor = id, context = context,
-                  startTimeNanos = time.nanoseconds, memoryPool, receive.payload, requestChannel.metrics)
-                requestChannel.sendRequest(req)
-                selector.mute(connectionId)
-                handleChannelMuteEvent(connectionId, ChannelMuteEvent.REQUEST_RECEIVED)
-              }
+            else if (channel.serverAuthenticationSessionExpired(time.nanoseconds)) {
+              channel.disconnect()
+              debug(s"Disconnected expired channel: $channel : $header")
+              expiredConnectionsKilledCount.record(null, 1, 0)
+            } else {
+              val connectionId = receive.source
+              val context = new RequestContext(header, connectionId, channel.socketAddress,
+                channel.principal, listenerName, securityProtocol)
+              val req = new RequestChannel.Request(processor = id, context = context,
+                startTimeNanos = time.nanoseconds, memoryPool, receive.payload, requestChannel.metrics)
+              requestChannel.sendRequest(req)
+              selector.mute(connectionId)
+              handleChannelMuteEvent(connectionId, ChannelMuteEvent.REQUEST_RECEIVED)
             }
           case None =>
             // This should never happen since completed receives are processed immediately after `poll()`
